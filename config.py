a61ee104831f93c944fc081b73b4b7c1a61a3779
Kristoffer Forss <kristoffer.forss@gmail.com>
2018-10-31 11:26:36 +0100

    Update config.py

--- config.py
+++ config.py
@@ -18,45 +18,54 @@
 
 class Config:
     """Configuration for IPSC_ZRT470"""
-    # Connection configuration
-    master = ' '            # IP/domain of master server
-    port = ' '              # Port of master server
-    passphrase = ' '        # Passphrase given by master server admin
+    # IP Connection configuration
+    master = " "            # IP/domain of master server
+    port = " "              # Port of master server
+    passphrase = " "        # Passphrase given by master server admin
     pingTime = 60           # How often to ping the master server in seconds
     numOfPings = 10         # How many dead pings before calling connection dead
     
+    # COM configuration
+    rxCOM = " "             # Serial port for reciever
+    rxBAUD = 19200          # Serial Baudrate for reciever
+    txCOM = " "             # Serial port for transmitter
+    txBAUD = 19200          # Serial Baudrate for transmitter
+    simplexCOM = " "        # Serial port for simplex mode 
+                                # (will be used if rxFreq == txFreq)
+    simplexBAUD = 19200     # Serial Baudrate for simplex mode
+    
     # IPSC configuration
-    signature = 'RPTC'      # The IPSC command for configuration
-    callsign = ' ' * 8      # 8 Byte ASCII
-    rptrId = '00000000'     # 3 Bytes registered DMR-ID for public repeaters,
+    signature = "RPTC"      # The IPSC command for configuration
+    callsign = " " * 8      # 8 Byte ASCII
+    rptrId = "00000000"     # 3 Bytes registered DMR-ID for public repeaters,
                                 # 4 Bytes for private repeaters, HEX
-    rxFreq = '434787500'    # RX frequency in Hz
-    txFreq = '434787500'    # TX frequency in Hz
-    txPwr = '00'            # TX power in dBm, decimal (00-99)
-    CC = '01'               # ColorCode/Systemcode of the repeater (01-15)
-    lat = '+00.0000'        # Latitude in ASCII (8 Digits including 4 decimals,
+    rxFreq = "434787500"    # RX frequency in Hz
+    txFreq = "434787500"    # TX frequency in Hz
+    txPwr = "00"            # TX power in dBm, decimal (00-99)
+    CC = "01"               # ColorCode/Systemcode of the repeater (01-15)
+    lat = "+00.0000"        # Latitude in ASCII (8 Digits including 4 decimals,
                                 # - and decimal elimiter)
-    long = '+000.0000'      # Latitude in ASCII (9 Digits including 4 decimals,
+    long = "+000.0000"      # Latitude in ASCII (9 Digits including 4 decimals,
                                 # - and decimal elimiter)
-    height = '000'          # Antenna height over ground level in meters (000-999)
-    loc = ' ' * 20          # The nominal location of the repeater. This is a
+    height = "000"          # Antenna height over ground level in meters (000-999)
+    loc = " " * 20          # The nominal location of the repeater. This is a
                                 # free form text field. (only ASCII, no special
                                 # country specific characters or HTML codes, URLs)
                                 # 20 Chars
-    desc = ' ' * 20         # Optional information about the repeater. This is
+    desc = " "* 20         # Optional information about the repeater. This is
                                 # a free form text field. (only ASCII, no 
                                 # special country specific characters or 
                                 # HTML codes, URLs, no advertisements or 
                                 # non-Amateur Radio content)
                                 # 20 Chars
-    URL = ' ' * 124         # Optional web page for the repeater or group.
+    URL = " " * 124         # Optional web page for the repeater or group.
                                 # This is a free form text field.
                                 # (no advertisements or non-Amateur Radio
                                 # related links) “http://” not required 
                                 # 124 Chars
-    sId = ' ' * 40          # Software-ID with version number (no HTML, 
+    sId = " " * 40          # Software-ID with version number (no HTML, 
                                 # no advertising, only identification
                                 # 40 Chars
-    pId = ' ' * 40          # Package-ID with version number and platform
+    pId = " " * 40          # Package-ID with version number and platform
                                 # (no HTML, no advertising, only identification)
                                 # 40 Chars
